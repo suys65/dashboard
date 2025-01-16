ea62531e70cb9ff6fc93a76b7ea40a207e1bf5f6
suys65 <suysinu65@gmail.com>
2025-01-16 22:11:02 +0900

    휴복학이랑 사이드바 전환

--- scripts.js
+++ scripts.js
@@ -320,12 +320,12 @@ document.addEventListener('DOMContentLoaded', function() {    //휴복학
     const major_leavereturn_btn1 = document.getElementById('major_leavereturn_btn1');
     const major_leavereturn_btn2 = document.getElementById('major_leavereturn_btn2');
     // 함수 호출로 이벤트 핸들러 설정
-    setupButtonHandlers(major_leavereturn_btn1, major_leavereturn_btn2, 'https://prod-apnortheast-a.online.tableau.com/t/inu_dashboard/views/_17367196468810/sheet0?:origin=card_share_link&:embed=n');
+    setupButtonHandlers(major_leavereturn_btn1, major_leavereturn_btn2, 'https://prod-apnortheast-a.online.tableau.com/t/inu_dashboard/views/_17370272905130/sheet0?:origin=card_share_link&:embed=n');
     
     const grade_leavereturn_btn1 = document.getElementById('grade_leavereturn_btn1');
     const grade_leavereturn_btn2 = document.getElementById('grade_leavereturn_btn2');
     // 함수 호출로 이벤트 핸들러 설정
-    setupButtonHandlers(grade_leavereturn_btn1, grade_leavereturn_btn2, 'https://prod-apnortheast-a.online.tableau.com/t/inu_dashboard/views/_17367196468810/sheet1?:origin=card_share_link&:embed=n');
+    setupButtonHandlers(grade_leavereturn_btn1, grade_leavereturn_btn2, 'https://prod-apnortheast-a.online.tableau.com/t/inu_dashboard/views/_17370272905130/sheet1?:origin=card_share_link&:embed=n');
 
     const major_term_btn1 = document.getElementById('major_term_btn1');
     const major_term_btn2 = document.getElementById('major_term_btn2');
